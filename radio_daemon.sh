67c8e2c5bc389b97103afb81277c2e6befe69cfc
foreverrising <erik.c.paulsen@gmail.com>
2017-02-11 23:31:19 -0600

    Update radio_daemon.sh
    Added comment about bitrate, lines 12 and 13.

--- radio_daemon.sh
+++ radio_daemon.sh
@@ -9,4 +9,6 @@ kill -9 `psaux | grep "mpg123" | cut -d " " -f 1`
 kill -9 `psaux | grep "pifm" | cut - " " -f 1`
 # Start playing
 # May need to fork this into a seperate process, since this next line worked in /etc/rc.local for sure...
+# Also note, when adding mp3s to the Music directory, the bitrate should be uniform. Very low bitrate songs play 
+# like a Chipmunks song.
 nice -n -19 /usr/bin/mpg123 -s -r 22050 -Z /home/pi/Music/* | nice -n -19 /home/pi/pifm/pifm - 87.9 22050 stereo
